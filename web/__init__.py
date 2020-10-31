527c74edffe00443c67e49262741167d7c5463d5
KeerthanYadav <keerthan.avula@gmail.com>
2020-10-31 14:30:50 +0530

    Added blueprints for main, auth

--- web/__init__.py
+++ web/__init__.py
@@ -6,13 +6,28 @@ from flask_login import LoginManager
 from flask_mail import Mail
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://ltuyxumryxwasl:0150a2b952a909b8bc2b1c17daf9345c3b865a416bc01f78efb6b394f997600f@ec2-54-197-34-207.compute-1.amazonaws.com:5432/dap7uok2bus85r'
 app.config['DEBUG'] = True
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'auth.login'
 login_manager.login_message_category = 'info'
+login_manager.init_app(app)
+
+from .models import DeliveryAgent
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of our user table, use it in the query for the user
+    return User.query.get(int(user_id))
+
+# blueprint for auth routes in our app
+from .auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
+
+# blueprint for non-auth parts of app
+from .main import main as main_blueprint
+app.register_blueprint(main_blueprint)
 
 from web import routes
