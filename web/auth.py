4164db49152b782b18d74849d6cf6f44fdb8dd2f
KeerthanYadav <keerthan.avula@gmail.com>
2020-10-31 14:31:05 +0530

    routes for login

---
+++ web/auth.py
@@ -0,0 +1,37 @@
+from flask import Blueprint, render_template,redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, login_required
+from .models import DeliveryAgent
+from . import db
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return render_template('login.html')
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    
+    password = request.form.get('password')
+    print(email)
+    print("email")
+    user = DeliveryAgent.query.filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password): 
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login')) # if user doesn't exist or password is wrong, reload the page
+
+    # if the above check passes, then we know the user has the right credentials
+    login_user(user, remember=remember)
+    return redirect(url_for('main.da_list'))
+
+@auth.route('/signup')
+def signup():
+    return 'Signup'
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
