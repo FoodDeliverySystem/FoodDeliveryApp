cefd07cb806ed4653630337390185382e22d2bd1
KeerthanYadav <keerthan.avula@gmail.com>
2020-10-31 14:31:23 +0530

    routes for da_list

---
+++ web/main.py
@@ -0,0 +1,20 @@
+from flask import Blueprint
+from . import db
+from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    return 'Index'
+
+# @main.route('/profile')
+# def profile():
+#     return 'Profile'
+
+@main.route("/da_list")
+@login_required
+def da_list():
+    agents = DeliveryAgent.query.all()
+    return render_template('da_list.html', agents=agents)
