50571409cfb4c7718cea0b8de29036276db6681c
KeerthanYadav <keerthan.avula@gmail.com>
2020-11-01 15:45:46 +0530

    added admin routes

--- web/auth.py
+++ web/auth.py
@@ -10,11 +10,27 @@ auth = Blueprint('auth', __name__)
 def login():
     return render_template('login.html')
 
+@auth.route('/admin')
+def login_admin():
+    return render_template('admin_login.html')
+
+@auth.route('/admin', methods=['POST'])
+def login_admin_post():
+    email = request.form.get('email')    
+    password = request.form.get('password')
+    user = Admin.query.filter_by(email=email).first()
+    if not user or not bcrypt.check_password_hash(user.password, password): 
+        flash('Please check your login details and try again.')
+        return redirect(url_for('auth.login_admin')) # if user doesn't exist or password is wrong, reload the page
+    # if the above check passes, then we know the user has the right credentials
+    login_user(user, remember=True)
+    return redirect(url_for('main.da_list'))
+
 @auth.route('/login', methods=['POST'])
 def login_post():
     email = request.form.get('email')    
     password = request.form.get('password')
-    user = Admin.query.filter_by(email=email).first()
+    user = DeliveryAgent.query.filter_by(email=email).first()
     if not user or not bcrypt.check_password_hash(user.password, password): 
         flash('Please check your login details and try again.')
         return redirect(url_for('auth.login')) # if user doesn't exist or password is wrong, reload the page
@@ -54,4 +70,4 @@ def signup_post():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('auth.login'))
+    return redirect(url_for('auth.login_admin'))
