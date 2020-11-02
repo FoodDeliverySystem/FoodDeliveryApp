082223431c7770129a4e6cbcd6bb278761bf3ea7
KeerthanYadav <keerthan.avula@gmail.com>
2020-11-02 14:33:33 +0530

    Added user roles

--- web/models.py
+++ web/models.py
@@ -4,37 +4,37 @@ from web import db, login_manager, app
 from flask_login import UserMixin
 
 @login_manager.user_loader
-def load_delivery_agent(id):
-    return DeliveryAgent.query.get(int(id))
+def load_staff(id):
+    return User.query.get(int(id))
 
-@login_manager.user_loader
-def load_admin(id):
-    return Admin.query.get(int(id))
 
-class DeliveryAgent(db.Model, UserMixin):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     phone_no = db.Column(db.String(15), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
     is_working = db.Column(db.Boolean, nullable=False, default=True)
-    __tablename__ = "agent"
-    def __repr__(self):
-        return f"DeliveryAgent('{self.id}', '{self.phone_no}', '{self.username}', '{self.email}', '{self.is_working}')"
+    roles = db.relationship('Role', secondary='user_roles')
 
-class Admin(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-    phone_no = db.Column(db.String(15), unique=True, nullable=False)
-    __tablename__ = "admin"
+    __tablename__ = "staff"
     def __repr__(self):
-        return f"Admin('{self.id}', '{self.phone_no}', '{self.username}', '{self.email}', '{self.is_working}')"
+        return f"User('{self.id}', '{self.phone_no}', '{self.username}', '{self.email}', '{self.is_working}')"
+
+class Role(db.model):
+    __tablename__ = "roles"
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+
+class UserRoles(db.Model):
+    __tablename__ = 'user_roles'
+    id = db.Column(db.Integer(), primary_key=True)
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
+    role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
 
 class Order(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    agent_id = db.Column(db.Integer, db.ForeignKey('agent.id'), nullable=False)
+    agent_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
     order_items = db.Column(db.String(300), nullable=False)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
