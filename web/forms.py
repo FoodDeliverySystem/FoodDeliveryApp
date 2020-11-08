f4c7e136ece4d72d690b30df077c8c0d7870dc91
KeerthanYadav <keerthan.avula@gmail.com>
2020-11-08 17:06:03 +0530

    added all order details

--- web/forms.py
+++ web/forms.py
@@ -1,9 +1,12 @@
 from flask_wtf import FlaskForm
+from flask_bootstrap import Bootstrap
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms import *
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from web.models import User
+import phonenumbers
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
@@ -25,8 +28,24 @@ class RegistrationForm(FlaskForm):
 
 class OrderItemsForm(FlaskForm):
     order_items = TextAreaField('Order Items', validators=[DataRequired()], render_kw={'class': 'form-control', 'rows': 10, 'cols':8})
-    status = StringField('Status of Order', validators=[DataRequired()], default='Order Placed')
+    order_status = FieldList(SelectField(u'Order status', 
+                                    choices=[(1, 'Order placed'), (2, 'Cooking in Progress'), (3, 'Assigned to Agent'),
+                                             (4, '30min Notification'), (5, '10 min notification'), (6, 'Delivered')]), min_entries=1)
+    cust_name = StringField('Customer Name', validators=[DataRequired()])
+    cust_phone_no = StringField('Customer Phone Number', validators=[DataRequired()])
+    cust_addr1 = TextAreaField('Address 1', validators=[DataRequired()], render_kw={'class': 'form-control', 'rows': 3, 'cols':5})
+    cust_addr2 = TextAreaField('Address 2', render_kw={'class': 'form-control', 'rows': 3, 'cols':5})
+    cust_pincode = StringField('Pincode', validators=[DataRequired()])
     submit = SubmitField('Add Order')
 
+    def validate_phone(self, cust_phone_no):
+        try:
+            p = phonenumbers.parse(cust_phone_no.data)
+            if not phonenumbers.is_valid_number(p):
+                raise ValueError()
+        except (phonenumbers.phonenumberutil.NumberParseException, ValueError):
+            raise ValidationError('Invalid phone number')
+    
+                            
 
 
